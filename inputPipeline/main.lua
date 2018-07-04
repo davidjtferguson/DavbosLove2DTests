471fd2cc934ae24ac728a43ce459eda8506bcae4
David Ferguson <davidjtferguson@gmail.com>
2018-07-04 21:07:58 +0100

    added states

--- inputPipeline/main.lua
+++ inputPipeline/main.lua
@@ -5,43 +5,77 @@ to get them in my head]]
 function love.load()
     drawColours = {0.5, 0.5, 0.5, 1}
     
+    rectPos = {x=10, y=10}
+
     test=""
 
-    -- bind to functions
-    bindings = {
-        showRed = red,
-        showGreen = green,
-        showBlue = blue,
-        showGrey = reset,
+    gameStates = {}
+
+    -- bind all controls for the colours gamestate
+    gameStates.colours = {
+        -- bind to functions
+        bindings = {
+            showRed = red,
+            showGreen = green,
+            showBlue = blue,
+            showGrey = reset,
+            toMovement = function() state = gameStates.movement end,
+        },
+        -- keyboard bindings
+        keys = {
+            r = "showRed",
+            g = "showGreen",
+            b = "showBlue",
+            n = "showBlue",
+        },
+        keysReleased = {
+            r = "showGrey",
+            g = "showGrey",
+            b = "showGrey",
+            n = "showGrey",
+            m = "toMovement",
+        },
+        -- controller bindings
+        buttons = {
+            b = "showRed",
+            a = "showGreen",
+            x = "showBlue",
+            back = "toMovement",
+        },
+        buttonsReleased = {
+            b = "showGrey",
+            a = "showGrey",
+            x = "showGrey",
+        }
     }
 
-    -- keyboard bindings
-    keys = {
-        r = "showRed",
-        g = "showGreen",
-        b = "showBlue",
-        n = "showBlue",
+    gameStates.movement = {
+        bindings = {
+            moveUp = function() rectPos.y=rectPos.y-5 end,
+            moveDown = function() rectPos.y=rectPos.y+5 end,
+            moveLeft = function() rectPos.x=rectPos.x-5 end,
+            moveRight = function() rectPos.x=rectPos.x+5 end,
+            toColours = function() state = gameStates.colours end,
+        },
+        keys = {
+            w = "moveUp",
+            s = "moveDown",
+            a = "moveLeft",
+            d = "moveRight",
+            c = "toColours",
+        },
+        keysReleased = {},
+        buttons = {
+            y = "moveUp",
+            a = "moveDown",
+            x = "moveLeft",
+            b = "moveRight",
+            back = "toColours",
+        },
+        buttonsReleased = {}
     }
 
-    keysReleased = {
-        r = "showGrey",
-        g = "showGrey",
-        b = "showGrey",
-        n = "showGrey",
-    }
-
-    -- controller bindings
-    buttons = {
-        b = "showRed",
-        a = "showGreen",
-        x = "showBlue",
-    }
-
-    buttonsReleased = {
-        b = "showGrey",
-        a = "showGrey",
-        x = "showGrey",
-    }
+    state = gameStates.colours
 end
 
 function love.update(dt)
@@ -49,15 +83,14 @@ function love.update(dt)
 end
 
 function love.draw()
-    -- i assume there's a nicer way to pass an appropriate obj to a function...
+    -- I assume there's a nicer way to pass an appropriate obj to a function...
     love.graphics.setColor(
         drawColours[1],
         drawColours[2],
         drawColours[3],
         drawColours[4])
 
-        love.graphics.rectangle('fill', 10,10,780,285)
-
+        love.graphics.rectangle('fill', rectPos.x,rectPos.y,780,285)
         
         love.graphics.print(test, 10, 400)
 end
@@ -65,14 +98,14 @@ end
 -- PROCESS INPUTS
 
 function inputHandler(input)
-    local action = bindings[input]
+    local action = state.bindings[input]
     if action then
         return action()
     end
 end
 
 function love.keypressed(k)
-    local binding = keys[k]
+    local binding = state.keys[k]
 
     test = k
     if binding then
@@ -83,17 +116,17 @@ function love.keypressed(k)
 end
 
 function love.keyreleased(k)
-    local binding = keysReleased[k]
+    local binding = state.keysReleased[k]
     return inputHandler(binding)
 end
 
 function love.gamepadpressed(gamepad, button)
-    local binding = buttons[button]
+    local binding = state.buttons[button]
     return inputHandler(binding)
 end
 
 function love.gamepadreleased(gamepad, button)
-    local binding = buttonsReleased[button]
+    local binding = state.buttonsReleased[button]
     return inputHandler(binding)
 end
 
