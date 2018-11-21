d524587ed005fb5a41c042aa5962fa762a386a58
David Ferguson <dferguson@scottlogic.co.uk>
2018-11-21 17:36:10 +0000

    fluxuate values based on time

--- vertexShaders/main.lua
+++ vertexShaders/main.lua
@@ -7,10 +7,26 @@ function love.load(args)
     vertex = love.filesystem.read('vertex.glsl')
 
     shader = love.graphics.newShader(pixel, vertex)
+
+    PI = 3.14
 end
 
  
 function love.draw()
+
+    -- normalised from 0 to PI... how do I normalise from -PI to PI?
+    --time = -PI * (love.timer.getTime() % 1)
+
+    -- get values from 0 to 1 alternating per second
+    theta = (love.timer.getTime() % 1)
+
+    -- normalise from 0 to 1 to -PI to PI
+    -- newvalue= (max'-min')/(max-min)*(value-max)+max'
+    time = (PI+PI)/(1-0)*(theta-1)+PI
+
+    print(time)
+
+    shader:send("time", time)
     love.graphics.setShader(shader)
 
     -- draw things
--- vertexShaders/vertex.glsl
+++ vertexShaders/vertex.glsl
@@ -1,8 +1,9 @@
 varying vec4 vpos;
+uniform float time;
  
 vec4 position( mat4 transform_projection, vec4 vertex_position )
 {
     vpos = vertex_position;
 
-    return transform_projection * vertex_position * vec4(0.5, 1.0, 1.0, 1.0);
+    return transform_projection * vertex_position * vec4(0.5, sin(time), 1.0, 1.0);
 }
